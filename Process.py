72b7be5c58b367ef34d92ede4baa98b3bcd9ad18
Fan Man Hon <lyndonf@jesu-c356.jesus.ox.ac.uk>
2020-02-06 21:47:04 +0000

    can shrink images

--- Process.py
+++ Process.py
@@ -4,12 +4,19 @@ from os import *
 
 def pixelize(img, maxSize): #img is from PIL.Image
     width, height = img.size
+    print(img.size)
     maxDim = max(width, height)
     newWidth = int(width*maxSize/maxDim)
     newHeight = int(height*maxSize/maxDim)
-    img.resize((newWidth, newHeight))
-    return img
+    print(newWidth, newHeight)
+    newImg = img.resize((newWidth, newHeight), Image.ANTIALIAS)
+    #img.show()
+    return newImg
 
-if __name__ = "__init__":
-    img = Image.open("testPic.jpg")
-    pixelize(img).save("testRes.jpg")
+if __name__ == "__main__":
+    bwimg = Image.open("bwPic.png")
+    newbwImg = pixelize(bwimg, 30)
+    newbwImg.save("bwRes.png","PNG")
+    cimg = Image.open("cPic.png")
+    newCImg = pixelize(cimg, 30)
+    newCImg.save("cRes.png","PNG")

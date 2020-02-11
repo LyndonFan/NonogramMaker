73486e36e0f7ef31b6e3be354ee1d5a2b9668865
Fan Man Hon <lyndonf@Fans-MacBook-Pro.local>
2020-02-11 23:07:58 +0000

    minor fixes

--- Process.py
+++ Process.py
@@ -70,13 +70,14 @@ def changeColors(img, n=1):
     #In case you want to preview the picture in your terminal...
     if len(colors)<10:
         print("\n".join(["".join(list(map(str,clusterNumbers[i*width:(i+1)*width]))) for i in range(height)]))
-    '''
+    
     #To see the colors
     if len(colors)>2:
         clrsImg = [[[x for asdf in range(100)] for x in colors] for qwer in range(100)]
         clrsImg = np.array(clrsImg)
         clrsImg = clrsImg.reshape(100, len(colors)*100, len(colors[0]))
         cv2.imwrite(str(hash(colors[0]))+"_"+str(len(colors))+"Colors.png", clrsImg)
+    '''
     return newImg
 
 #for testing purposes

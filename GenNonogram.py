ea8fe943c96562d296d696616462f6b4eb1668ef
Fan Man Hon <lyndonf@jesu-c325.jesus.ox.ac.uk>
2020-02-13 19:25:46 +0000

    done command line interface

--- GenNonogram.py
+++ GenNonogram.py
@@ -1,30 +1,36 @@
 from Process import *
 from Convert import *
-import sys, getopt
+import argparse
 from os import *
 import cv2
 
-def main(argv):
+def main(fileName, maxDim=80, toBW = False, numColors = "", getResult = True):
 
-    inputImg = ""
-    maxDim = 80
-
+    print("Processing "+fileName+"...")
     try:
-        opts, args = getopt.getopt(argv,"i:d:c:bw:n:t",["ifile=","ofile="])
-    except getopt.GetoptError:
-        print 'test.py -i <inputfile> [-d] [-c] [-bw] [-n]'
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt in ("-i", "--ifile"):
-            inputImg = cv2.imread(arg)
-        elif opt in ("-s", "--maxDim"):
-            try:
-                maxDim = int(arg)
-                assert maxDim>0, "The maximum dimension should be positive: "+str(maxDim)
-            except:
-                print("Using 80 as maximum dimension...")
-                maxDim = 80
-        elif 
+        processed = cv2.imread(fileName)
+    except:
+        print(fileName + "is not a path to a valid image")
+        sys.exit()
+    processed = pixelize(processed, maxDim, toBW)
+    processed = changeColors(processed, n = numColors)
+    if getResult:
+        cv2.imwrite("_Pic"+fileName,processed)
+    print("Getting nonogram...")
+    bgColor = getBackgroundColor(processed)
+    clues = getClues(processed, bgColor)
+    height, width = processed.shape[:2]
+    product = drawGrid(clues, (width, height), bgColor)
+    product = drawClues(np.float32(product), clues, (width, height))
+    cv2.imwrite("_Pix"+fileName, product)
+    print("Done!")
 
-    print 'Input file is "', inputfile
-    print 'Output file is "', outputfile
+parser = argparse.ArgumentParser(description = "Make a nonogram from a picture.")
+parser.add_argument("image",nargs=1,help="path to image to be processed")
+parser.add_argument("-d","--maxDim",default=80,nargs=1,type=int,help="maximum no. of squares in any dimension in the nonogram (default = 80)")
+parser.add_argument("-b","--blackAndWhite",action="count",help="indicate the image should be converted to black and white")
+parser.add_argument("-n","--numColor","--numColour",nargs="?",type=int,default=0,help="positive integer to denote the number of colors that should appear in your nonogram IN ADDITION to your background color.\nA 0 or negative integer means the algorithm will choose this number for you. \n (default = 0)")
+parser.add_argument("-r","--getResult",nargs="?",type=bool,default=True,help="boolean to indicate whether you want the finished picture (default = True)")
+args = parser.parse_args()
+print(args)
+main(args.image[0], args.maxDim[0], args.blackAndWhite, args.numColor, args.getResult)
